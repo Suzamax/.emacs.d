9eaf781dd129ffc0ff2ae08e194308ceb29161aa
Carlos Cañellas Tovar <ratiosu.cct@gmail.com>
2021-10-31 11:30:14 +0100

    Remove debris. If something fails just HMU, maybe JS2...

--- init.el
+++ init.el
@@ -124,17 +124,3 @@
 (ox-extras-activate '(latex-header-blocks ignore-headlines))
 
 (add-to-list 'default-frame-alist '(font . "Cascadia Code PL 15" ))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(olivetti exec-path-from-shell org-sidebar vs-dark-theme vscode-icon dap-java yasnippet which-key vertico use-package treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired treemacs-evil smex plantuml-mode org-plus-contrib orderless ns-auto-titlebar lsp-ui lsp-java js2-mode highlight-symbol highlight-indent-guides function-args flycheck doom-themes doom-modeline dashboard company-quickhelp company-prescient color-identifiers-mode centaur-tabs all-the-icons-dired))
- '(warning-suppress-log-types '((comp))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
