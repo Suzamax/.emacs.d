88870a11f921f007f2dc7022e62aad8024ed2efe
Carlos Cañellas Tovar <carlos@Mac-mini-de-Carlos.local>
2023-06-04 13:53:17 +0200

    first commit

---
+++ init.el
@@ -0,0 +1,17 @@
+(org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("e7820b899036ae7e966dcaaec29fd6b87aef253748b7de09e74fdc54407a7a02" default))
+ '(purescript-mode-hook
+   '(capitalized-words-mode turn-on-eldoc-mode turn-on-purescript-indentation)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'upcase-region 'disabled nil)
